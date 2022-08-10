25ab40e8e5c01616f800e84e04b9eddc04918bea
test <valentyn.novikov@gmail.com>
2022-08-10 16:14:06 +0300

    small refactoring

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useLayoutEffect } from 'react';
 
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet } from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withRepeat,
   withTiming,
   cancelAnimation,
-  Easing
-} from "react-native-reanimated";
+  Easing,
+} from 'react-native-reanimated';
 
-const LoadingSpinner = ({showSpinner}) => {
+const LoadingSpinner = ({ showSpinner }: { showSpinner: boolean }) => {
   const rotation = useSharedValue(0);
   const animatedStyles = useAnimatedStyle(() => {
     return {
       transform: [
         {
-          rotate: `${rotation.value}deg`
-        }
-      ]
+          rotate: `${rotation.value}deg`,
+        },
+      ],
     };
   }, [rotation.value]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     rotation.value = withRepeat(
       withTiming(360, {
         duration: 1000,
@@ -31,27 +31,27 @@ const LoadingSpinner = ({showSpinner}) => {
       -1,
     );
     return () => cancelAnimation(rotation);
-  }, []);
+  }, [showSpinner]);
+
+  if (!showSpinner) {
+    return null;
+  }
 
   return (
-    <>
-      {showSpinner && (
-        <View style={styles.container}>
-          <Animated.View style={[styles.spinner, animatedStyles]} />
-        </View>
-      )}
-    </>
+    <View style={styles.container}>
+      <Animated.View style={[styles.spinner, animatedStyles]} />
+    </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     zIndex: 2,
   },
   spinner: {
@@ -59,10 +59,10 @@ const styles = StyleSheet.create({
     width: 60,
     borderRadius: 30,
     borderWidth: 7,
-    borderTopColor: "rgba(69,97,218,0.35)",
-    borderRightColor: "rgba(69,97,218,0.35)",
-    borderBottomColor: "rgba(69,97,218,0.35)",
-    borderLeftColor: "rgba(255,255,255,0.84)"
-  }
+    borderTopColor: 'rgba(69,97,218,0.35)',
+    borderRightColor: 'rgba(69,97,218,0.35)',
+    borderBottomColor: 'rgba(69,97,218,0.35)',
+    borderLeftColor: 'rgba(255,255,255,0.84)',
+  },
 });
 export default LoadingSpinner;
