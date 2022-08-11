086d2c28bab3697df2a79fca7c3e3f6071181479
test <valentyn.novikov@gmail.com>
2022-08-11 15:35:08 +0300

    refactoring of loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -9,8 +9,17 @@ import Animated, {
   cancelAnimation,
   Easing,
 } from 'react-native-reanimated';
+import { scaleSize } from "@utils/scaleSize";
 
-const LoadingSpinner = ({ showSpinner }: { showSpinner: boolean }) => {
+interface LoginSpinnerProps {
+  showSpinner: boolean;
+  size?: number;
+}
+
+const LoadingSpinner: React.FC<LoginSpinnerProps> = ({
+  showSpinner,
+  size = 60,
+}) => {
   const rotation = useSharedValue(0);
   const animatedStyles = useAnimatedStyle(() => {
     return {
@@ -22,14 +31,25 @@ const LoadingSpinner = ({ showSpinner }: { showSpinner: boolean }) => {
     };
   }, [rotation.value]);
 
+  const dynamicStyles = StyleSheet.create({
+    innerStyle: {
+      height: scaleSize(size),
+      width: scaleSize(size),
+      borderRadius: scaleSize(size / 2),
+      borderWidth: scaleSize((size * 7) / 60),
+    },
+});
+
   useLayoutEffect(() => {
-    rotation.value = withRepeat(
-      withTiming(360, {
-        duration: 1000,
-        easing: Easing.linear,
-      }),
-      -1,
-    );
+    if (showSpinner) {
+      rotation.value = withRepeat(
+        withTiming(360, {
+          duration: 1000,
+          easing: Easing.linear,
+        }),
+        99999999,
+      );
+    }
     return () => cancelAnimation(rotation);
   }, [showSpinner]);
 
@@ -38,27 +58,13 @@ const LoadingSpinner = ({ showSpinner }: { showSpinner: boolean }) => {
   }
 
   return (
-    <View style={styles.container}>
-      <Animated.View style={[styles.spinner, animatedStyles]} />
-    </View>
+    <Animated.View
+      style={[styles.spinner, dynamicStyles.innerStyle, animatedStyles]}
+    />
   );
 };
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 2,
-  },
   spinner: {
-    height: 60,
-    width: 60,
-    borderRadius: 30,
-    borderWidth: 7,
     borderTopColor: 'rgba(69,97,218,0.35)',
     borderRightColor: 'rgba(69,97,218,0.35)',
     borderBottomColor: 'rgba(69,97,218,0.35)',
