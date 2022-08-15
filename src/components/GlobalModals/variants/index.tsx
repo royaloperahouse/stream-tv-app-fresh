3cc7f4c5a7856ffcd52c3ef3538e6268683c9a3c
test <valentyn.novikov@gmail.com>
2022-08-15 15:56:12 +0300

    added global modals components

---
+++ src/components/GlobalModals/variants/index.tsx
@@ -0,0 +1,6 @@
+export { default as NotSubscribedModal } from './notSubscribedModal';
+export { default as СontinueWatchingModal } from './continueWatchingModals';
+export { default as ErrorModal } from './errorModal';
+export { default as WarningOfExitModal } from './warningOfExitModal';
+export { default as RentalStateStatusModal } from './rentalStateStatusModal';
+// export { default as PlayerModal } from './playerModal';
---
+++ src/components/GlobalModals/variants/playerModal.ts
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import Player, { TPlayerProps } from '@components/Player';
+
+// const PlayerModal: React.FC<any> = props =>  {...props} />;
+//
+// export default PlayerModal;
