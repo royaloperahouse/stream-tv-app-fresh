e3b4419cde7a6db39aa29deaefd94f5187000b87
valentyn.novikov <valentyn.novikov@avenga.com>
2022-09-16 16:51:35 +0300

    change app name, version, fix for placeholder images

--- src/components/RohImage.tsx
+++ src/components/RohImage.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react';
 import FastImage, { ResizeMode } from 'react-native-fast-image';
 import PlaceholderLandscape from '../assets/image-placeholder-landscape.svg';
 import PlaceholderPortrait from '../assets/image-placeholder-portrait.svg';
+import { StyleSheet, View } from 'react-native';
 
 type TRohImageProps = {
   resizeMode: ResizeMode;
@@ -26,14 +27,39 @@ const RohImage: FC<TRohImageProps> = ({
   }
 
   return (
-    <FastImage
-      resizeMode={resizeMode}
-      style={style}
-      source={{
-        uri: source,
-      }}
-      onError={() => setIsError(true)}
-    />
+    <View style={style}>
+        <View style={styles.overlay}>
+      {
+        isPortrait ? (
+            <PlaceholderPortrait width={style.width} height={style.height} />
+        ) : (
+            <PlaceholderLandscape width={style.width} height={style.height} />
+        )
+      }
+        </View>
+
+      <FastImage
+            resizeMode={resizeMode}
+            style={[style, styles.container]}
+            source={{
+              uri: source,
+            }}
+            onError={() => setIsError(true)}
+        />
+      </View>
   );
 };
+const styles = StyleSheet.create({
+      container: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0,
+      },
+      overlay: {
+          opacity: 0.1
+      }
+    }
+)
 export default RohImage;
