9d105fe1ab76cc14d98cf747a8b616d6810941e5
j4ndrw <murjan_andreialin@yahoo.com>
2022-10-03 13:48:04 +0300

    RD-8834 refactor: Make the `useMyList` hook compatible with BE services

--- src/hooks/useMyList.tsx
+++ src/hooks/useMyList.tsx
@@ -1,26 +1,32 @@
 import { useState, useCallback, useRef } from 'react';
 import { useFocusEffect } from '@react-navigation/core';
 import { getMyList } from '@services/myList';
+import { useAppSelector } from './redux';
+import { customerIdSelector } from 'services/store/auth/Selectors';
 
 export const useMyList = (): { data: Array<string>; ejected: boolean } => {
+  const customerId = useAppSelector(customerIdSelector);
+
   const ejected = useRef<boolean>(false);
   const [myList, setMyList] = useState<Array<string>>([]);
   const mountedRef = useRef<boolean | undefined>(false);
   useFocusEffect(
     useCallback(() => {
       mountedRef.current = true;
-      getMyList().then(items => {
-        if (mountedRef && mountedRef.current) {
-          ejected.current = true;
-          setMyList(items);
-        }
-      });
+      if (customerId) {
+        getMyList(customerId.toString()).then(items => {
+          if (mountedRef && mountedRef.current) {
+            ejected.current = true;
+            setMyList(items);
+          }
+        });
+      }
       return () => {
         if (mountedRef && mountedRef.current) {
           mountedRef.current = false;
         }
       };
-    }, []),
+    }, [customerId]),
   );
   return { data: myList, ejected: ejected.current };
 };
