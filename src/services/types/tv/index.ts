1fa58b3fee052ca1e20c21b35c7ea2a45ef5aa8f
j4ndrw <murjan_andreialin@yahoo.com>
2022-10-03 15:27:31 +0300

    RD-8834 chore: Create type defs for Watch Status + API responses

--- src/services/types/tv/index.ts
+++ src/services/types/tv/index.ts
@@ -2,4 +2,20 @@ interface Event {
   id: string;
 }
 
+export interface WatchStatusItem {
+  position: string;
+}
+
+export interface SearchItem {
+  createdAt: Date;
+}
+
 export type MyList = Event[];
+
+export interface WatchStatus {
+  [videoId: string]: WatchStatusItem | undefined;
+}
+
+export interface SearchHistory {
+  [text: string]: SearchItem | undefined;
+}
