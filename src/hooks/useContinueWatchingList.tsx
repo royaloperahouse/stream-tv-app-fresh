9c390430d67cfbd96ff0a16d345db7c3f4d8ecec
j4ndrw <murjan_andreialin@yahoo.com>
2022-10-04 15:32:47 +0300

    RD-8834 feat: Added video details retriever func to unique event getter

--- src/hooks/useContinueWatchingList.tsx
+++ src/hooks/useContinueWatchingList.tsx
@@ -1,14 +1,27 @@
-import { useState, useCallback, useRef } from 'react';
+import { useState, useCallback, useRef, useContext, useMemo } from 'react';
 import { useFocusEffect } from '@react-navigation/core';
 import { getListOfUniqueEventId } from '@services/bitMovinPlayer';
 import { useAppSelector } from './redux';
 import { customerIdSelector } from 'services/store/auth/Selectors';
+import { isProductionEvironmentSelector } from 'services/store/settings/Selectors';
+
+import * as Prismic from '@prismicio/client';
+import { getVideoDetails } from 'services/prismicApiClient';
 
 export const useContinueWatchingList = (): {
   data: Array<string>;
   ejected: boolean;
 } => {
   const customerId = useAppSelector(customerIdSelector);
+  const isProduction = useAppSelector(isProductionEvironmentSelector);
+  const videoDetailsRetriever = useMemo(
+    () => (videoIDs: string[]) =>
+      getVideoDetails({
+        queryPredicates: [Prismic.Predicates.in('document.id', videoIDs)],
+        isProductionEnv: isProduction,
+      }),
+    [isProduction],
+  );
 
   const ejected = useRef<boolean>(false);
   const [continueWatchingList, setContinueWatchingList] = useState<
@@ -19,7 +32,7 @@ export const useContinueWatchingList = (): {
     useCallback(() => {
       mountedRef.current = true;
       if (customerId) {
-        getListOfUniqueEventId(customerId).then(items => {
+        getListOfUniqueEventId(customerId, videoDetailsRetriever).then(items => {
           if (mountedRef.current) {
             ejected.current = true;
             setContinueWatchingList(items);
