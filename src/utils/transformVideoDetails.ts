f1e5af717faf7d26199490edf377d2fb4185743d
Roman Yatsynych <rom.yatsynych@gmail.com>
2023-07-26 12:34:37 +0300

    fixed duration for digital event details

--- src/utils/transformVideoDetails.ts
+++ src/utils/transformVideoDetails.ts
@@ -11,21 +11,49 @@ export function transformVideoDetails(videoDetails) {
       : videoDetails.data.preview_image.tray.url,
   };
 
-  let vs_running_time_summary = null;
-  let minutes = videoDetails.data.video
-    ? Math.floor(videoDetails.data.video.duration / 60)
-    : 0;
-  let hours = 0;
-  vs_running_time_summary = `${minutes} minutes`;
-  if (minutes > 60) {
-    hours = Math.floor(minutes / 60);
-    minutes = minutes - hours * 60;
-    if (hours === 1) {
-      vs_running_time_summary = `${hours} hour and ${minutes} minutes`;
+  let vs_running_time_summary = '';
+  let seconds = videoDetails.data.video ? videoDetails.data.video.duration : 0;
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  if (seconds > 0 && minutes < 1) {
+    vs_running_time_summary = `${seconds} seconds`;
+  }
+
+  if (minutes > 0 && hours < 1) {
+    seconds = seconds - minutes * 60;
+    if (minutes === 1) {
+      if (seconds > 0) {
+        vs_running_time_summary = `${minutes} minute and ${seconds} seconds`;
+      } else {
+        vs_running_time_summary = `${minutes} minute`;
+      }
     } else {
-      vs_running_time_summary = `${hours} hours and ${minutes} minutes`;
+      if (seconds > 0) {
+        vs_running_time_summary = `${minutes} minutes and ${seconds} seconds`;
+      } else {
+        vs_running_time_summary = `${minutes} minutes`;
+      }
     }
   }
+
+  if (hours > 0) {
+    minutes = minutes - hours * 60;
+    if (hours === 1) {
+      if (minutes > 0) {
+        vs_running_time_summary = `${hours} hour and ${minutes} minutes`;
+      } else {
+        vs_running_time_summary = `${hours} hour`;
+      }
+    } else {
+      if (minutes > 0) {
+        vs_running_time_summary = `${hours} hours and ${minutes} minutes`;
+      } else {
+        vs_running_time_summary = `${hours} hours`;
+      }
+    }
+  }
+  console.log(vs_running_time_summary);
   return {
     id: videoDetails.id,
     type: 'digital_event_video',
