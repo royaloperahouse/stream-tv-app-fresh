578c2fd3fc4322272d0a5ab262f1821d5859391f
Roman Yatsynych <rom.yatsynych@gmail.com>
2023-11-13 17:13:47 +0200

    rolled back metro config

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,25 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 /**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
  *
- * @type {import('metro-config').MetroConfig}
+ * @format
  */
-const config = {};
+const { getDefaultConfig } = require('metro-config');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = (async function () {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
